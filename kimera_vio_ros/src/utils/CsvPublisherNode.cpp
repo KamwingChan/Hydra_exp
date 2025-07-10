622f7715908bccedc8f9bc0b82192cd74b285f59
Kamwing CHAN <130223084+KamwingChan@users.noreply.github.com>
2025-07-10 20:34:49 +0800

    Delete kimera_vio_ros directory

--- kimera_vio_ros/src/utils/CsvPublisherNode.cpp
+++
@@ -1,9 +0,0 @@
-#include "kimera_vio_ros/utils/CsvPublisher.h"
-
-int main(int argc, char** argv) {
-  google::ParseCommandLineFlags(&argc, &argv, true);
-  google::InitGoogleLogging(argv[0]);
-  ros::init(argc, argv, "csv_publisher_node");
-  VIO::utils::CsvPublisher csv_publisher;
-  ros::spin();
-}
